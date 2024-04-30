9ca37fc9fd579186522d6d65ebaeea2fe2a5d5fa
DILEEP <96054518+A-DILEEP@users.noreply.github.com>
2024-04-30 09:07:32 +0530

    Update portfolio.js
    Updated footer js

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    
+   
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
     } else {
       $(".navbar").removeClass("sticky");
     }
-
-  
     if (this.scrollY > 500) {
       $(".scroll-up-btn").addClass("show");
     } else {
@@ -17,14 +15,13 @@ $(document).ready(function () {
 
   $(".scroll-up-btn").click(function () {
     $("html").animate({ scrollTop: 0 });
-  
+
     $("html").css("scrollBehavior", "auto");
   });
 
   $(".navbar .menu li a").click(function () {
     $("html").css("scrollBehavior", "smooth");
   });
-
 
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
@@ -40,7 +37,7 @@ $(document).ready(function () {
     loop: true,
   });
   var typed = new Typed(".typing-3", {
-    strings: ["Connect with me on :)"],
+    strings: ["Connect with me on :)", "Created by @Dileep"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
@@ -54,7 +51,6 @@ $(document).ready(function () {
     backSpeed: 60,
     loop: true,
   });
-
 
   $(".carousel").owlCarousel({
     margin: 20,
